07547643d2300206357f1556861bd40de2e41a2b
tinorterry <58237976+tinorterry@users.noreply.github.com>
2020-02-10 23:48:08 -0500

    Update Team.java

--- Team.java
+++ Team.java
@@ -1,6 +1,6 @@
 /**
   
- @author  
+ @author  Tin Fung
  */
 public class Team 
 {
@@ -11,41 +11,79 @@ public class Team
    
    public Team()
    {
+	   this.team= new TeamMember[4];
+	   int numMembers=0;
+	   
       //this is the default constructor
    }
    
    private int find(TeamMember m)
    {
-       
+	   for(int i=0;i<team.length;i++) {
+		   if(m.equals(team[i])) {
+			   return i;
+		   }
+	   }
+	   
+       return NOT_FOUND;
    }
    
    private void grow()
    {
+	   TeamMember [] temp=new TeamMember[this.numMembers+GROW_SIZE];
+	   for(int i=0;i<this.numMembers;i++) {
+		   temp[i]=this.team[i];
+	   }
+	   this.team=temp;
        
    }
    
    public boolean isEmpty()
-   {
-       
+   {if(this.numMembers==0||this.team==null) {
+	   return true;
+   }
+       return false;
    }
    
    public void add(TeamMember m)
-   {     
-            
+   {
+	   if(numMembers==this.team.length) {
+		   this.grow();
+	   }
+	   this.team[this.numMembers]=m;
+		   
+            this.numMembers++;
    }
    
    public boolean remove(TeamMember m)
    {
-       
+	  int temp= this.find(m);
+	   if(temp!=-1) {
+		   this.team[temp]=this.team[this.numMembers-1];
+		   this.team[this.numMembers]=null;
+		   this.numMembers--;
+		   return true;
+		   
+	   }
+
+	   
+       return false;
    } 
    
    public boolean contains(TeamMember m)
    {
+	   if(find(m)!=-1) {
+		   return true;
+	   }
+	   return false;
       
    } 
    
    public void print()
    {
+	   for(int i=0;i<this.numMembers;i++) {
+		   System.out.println(this.team[i].toString());
+	   }
       //set up a for loop and call the toString() method
    } 
 }
